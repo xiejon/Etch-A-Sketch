ff162af277bae4adaa38d1168fed0bd40227e258
xiejon <jxie77@gmail.com>
2022-04-26 17:22:16 +0100

    added button for user-generated rows

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,5 +1,8 @@
 // create 16 blank divs in html
 const container = document.querySelector(".container")
+const resetButton = document.querySelector("#reset");
+
+// default 16x16 grid
 
 for (let i = 0; i < (16 * 16); i++) {
     const box = document.createElement("div");
@@ -7,11 +10,54 @@ for (let i = 0; i < (16 * 16); i++) {
     box.addEventListener("mouseover", function(e) {
         e.target.style.backgroundColor = 'black';
     });
-  
+        
     container.appendChild(box);
 
-    const resetButton = document.querySelector("#reset");
+    clearGridButton(box);
+}
+
+
+// new grid according to user input
+const newGrid = document.querySelector("#grid");
+let elementNum = container.childElementCount;
+
+function clearGridButton(box) {
     resetButton.addEventListener("click", function() {
         box.style.backgroundColor = '';
     });
 }
+
+function deleteGrid(num) {
+    for (let i = 0; i < num; i++) {
+        const box = document.querySelector(".box");
+        box.remove();
+    }
+}
+
+newGrid.addEventListener("click", function() {
+    let boxNum = prompt("Number of rows?");
+
+    if (boxNum < 1 || boxNum > 100) {
+        alert("Please enter a number between 1 and 100.")
+        return;
+    } else {
+        deleteGrid(elementNum); 
+
+        for (let i = 0; i < (boxNum * boxNum); i++) {
+            const box = document.createElement("div");
+            box.classList.add("box");
+            box.addEventListener("mouseover", function(e) {
+                e.target.style.backgroundColor = 'black';
+            });
+            container.appendChild(box);
+        
+            clearGridButton(box);
+
+        }
+        container.style.gridTemplateColumns = `repeat(${boxNum}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${boxNum}, 1fr)`;
+
+        elementNum = boxNum * boxNum;
+    } 
+});
+
